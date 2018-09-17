4fb0f98c782e892b3787ed51156611ae07e0b55a
Potamianos <Potamianos@users.noreply.github.com>
2018-09-17 19:48:02 -0400

    added image that follows mouse at a delay

--- Exercises/Exercise 1/js/script.js
+++ Exercises/Exercise 1/js/script.js
@@ -6,9 +6,13 @@
 // One moves linearly down the screen.
 // One moves toward the mouse cursor.
 
-
 // The image of a clown face
 var clownImage;
+//image of meme
+var memeImage
+//position of meme image
+var memeImageX
+var memeImageY
 //The Image of pixel Dirt
 var dirtImage;
 //X and Y Position of Pixel Dirt
@@ -33,6 +37,7 @@ function preload() {
   dirtImage = loadImage("assets/images/dirt.jpg");
   clownImage = loadImage("assets/images/clown.png");
   feltTextureImage = loadImage("assets/images/black-felt-texture.png");
+  memeImage = loadImage("assets/images/meme.jpg");
 }
 
 
@@ -43,7 +48,9 @@ function preload() {
 function setup() {
   // Create our canvas
   createCanvas(640,640);
-
+  // start meme image at center of canvas
+  memeImageX = width/2
+  memeImageY = height/2
   // Start the clown image at the centre of the canvas
   clownImageX = width/2;
   clownImageY = height/2;
@@ -74,6 +81,7 @@ function draw() {
   //display image
   image(dirtImage,dirtImageX,dirtImageY);
 
+
   // Display the felt image
   image(feltTextureImage,feltTextureImageX,feltTextureImageY);
 
@@ -82,9 +90,19 @@ function draw() {
   // Calculate the distance in X and in Y
   var xDistance = mouseX - clownImageX;
   var yDistance = mouseY - clownImageY;
+  var x2Distance = mouseX - memeImageX;
+  var y2Distance = mouseY - memeImageY;
+
   // Add 1/10th of the x and y distance to the clown's current (x,y) location
   clownImageX = clownImageX + xDistance/10;
   clownImageY = clownImageY + yDistance/10;
+  // Add 1/50th of the x and y distance to the meme's current (x,y) location
+  memeImageX = memeImageX + x2Distance/50;
+  memeImageY = memeImageY + y2Distance/50;
+
+  //display memeImage
+  image(memeImage,memeImageX,memeImageY,100,100);
+
 
   // Display the clown image
   image(clownImage,clownImageX,clownImageY);
